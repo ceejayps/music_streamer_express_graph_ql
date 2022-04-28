82af89a336a6e94c0d08eba38838f75462acf5ee
ceejayps <ceejayps1308@gmail.com>
2022-04-28 10:18:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,27 @@ const { graphql, buildSchema } = require('graphql')
 const schema = buildSchema(`
 type Query {
 hello: String
+artist:Artist
+} 
+type Artist {
+    id: Int
+    name: String
+    albums:[Album]
+  
 }
+type Album {
+  id: Int
+  name: String
+  songs:[Song]
+}
+type Song {
+  id: Int
+  name: String
+  coverUrl: String
+  SongUrl: String
+ 
+}
+
 
 `);
 const root = { hello: () => 'Hello world!'}
