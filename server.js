9ee50048fa5f8566ab9915f897476fd3e8ca1f2f
ceejayps <ceejayps1308@gmail.com>
2022-04-28 10:42:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,64 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { graphql, buildSchema } = require('graphql')
+
+const artists = [{
+  id:100001,
+  name:"Ariana Grande",
+  albums:[
+    {
+    name:"Yours Truly",
+    songs:[
+      {
+      name: "Hearts Up",
+      coverUrl: "String",
+      SongUrl: "String"}]
+  },
+  {
+    name:"My Everything",
+    songs:[
+      {
+      name: "Hearts Up",
+      coverUrl: "String",
+      SongUrl: "String"}]
+  },
+  {
+    name:"Dangerous Woman",
+    songs:[
+      {
+      name: "Hearts Up",
+      coverUrl: "String",
+      SongUrl: "String"}]
+  },
+  {
+    name:"Sweetener",
+    songs:[
+      {
+      name: "Hearts Up",
+      coverUrl: "String",
+      SongUrl: "String"}]
+  },
+  {
+    name:"Thank You, Next",
+    songs:[
+      {
+      name: "Hearts Up",
+      coverUrl: "String",
+      SongUrl: "String"}]
+  },
+  {
+    name:"Positions",
+    songs:[
+      {
+      name: "Hearts Up",
+      coverUrl: "String",
+      SongUrl: "String"}]
+  }
+]
+}
+
+]
+
 const schema = buildSchema(`
 type Query {
 hello: String
@@ -27,7 +85,14 @@ type Song {
 
 
 `);
-const root = { hello: () => 'Hello world!'}
+
+
+const root = { 
+  hello: () => 'Hello world!',
+  artist:()=>{
+   return artists[0]
+  }
+}
 
 const app = express();
 
