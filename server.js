d6df391c1bbd313beae2968a35ea949a8b644781
ceejayps <ceejayps1308@gmail.com>
2022-04-27 20:12:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const { graphql, buildSchema } = require('graphql')
+const schema = buildSchema(`
+type Query {
+hello: String
+}
+
+`);
+const root = { hello: () => 'Hello world!'}
 
 const app = express();
 
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: MyGraphQLSchema,
+    schema,
+    rootValue:root,
     graphiql: true,
   }),
 );
